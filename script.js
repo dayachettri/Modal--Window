8cfd77e866a9053232230650b63783ddeb38b8a8
Daya Chettri <Dayasagar073@gmail.com>
2022-10-26 11:15:16 +0530

    Refactor the code & remove repetitive code

--- script.js
+++ script.js
@@ -5,21 +5,27 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-/* Looping over the nodelist which contains all of the 3 buttons and adding eventlisterner function to all of them  */
+// Fn to remove the classes which hide the modal window
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Fn to add back the classes which hide the modal window
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Step:1 Looping over the nodelist which contains all of the 3 buttons and adding eventlisterner function to all of them
+// Step:2 Open the modal window on a click event on btnsOpenModal
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    /* Removing the classes on a click event on btnsOpenModal which are hiding the modal window, we dont need (.) before the class name in this case */
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
 //Close the modal window on a click event on btnsCloseModal
-btnsCloseModal.addEventListener('click', function () {
-  /* Adding back the classes on a click event on btnsCloseModal which are hiding the modal window  */
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnsCloseModal.addEventListener('click', closeModal);
+
+//Close the modal window on a click event on overlay: basically outside the modal window
+overlay.addEventListener('click', closeModal);
