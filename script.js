a86521dc6836c55c6fcf49c5c407cbeac01fdad0
Daya Chettri <Dayasagar073@gmail.com>
2022-10-26 10:48:15 +0530

    Add code to display the modal window on a click event on btnsOpenModal

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Storing needed classes in a variable
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+/* Looping over the nodelist which contains all of the 3 buttons and adding eventlisterner function to all of them  */
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button Clicked');
+    /* Removing the classes on a click event on btnsOpenModal which are hiding the modal window, we dont need (.) before the class name in this case */
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
